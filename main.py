8ce50701cd66485159a3fc7b737b3e0810f1ad45
Inksplat58 <124327110+Inksplat58@users.noreply.github.com>
2024-04-01 09:23:10 -0400

    wrote code

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from wonderwords import RandomWord
+import os, time
+
+r = RandomWord()
+word = r.word(word_min_length=6, word_max_length=6)
+print("welcome to wordle but with 6 letters instead of 5")
+time.sleep(0.5)
+print("you have 7 guesses")
+time.sleep(0.5)
