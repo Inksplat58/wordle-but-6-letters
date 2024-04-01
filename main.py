aa37a9e021657b161bf20d9bdfa129e4e91d5962
Inksplat58 <124327110+Inksplat58@users.noreply.github.com>
2024-04-01 10:43:25 -0400

    wrote code

--- main.py
+++ main.py
@@ -4,6 +4,5 @@ import os, time
 r = RandomWord()
 word = r.word(word_min_length=6, word_max_length=6)
 print("welcome to wordle but with 6 letters instead of 5")
-time.sleep(0.5)
 print("you have 7 guesses")
-time.sleep(0.5)
+print (word)
