1c6af9bac3f0f598d1c749c3883a2dac7dcde675
Inksplat58 <124327110+Inksplat58@users.noreply.github.com>
2024-04-02 14:49:23 -0400

    wrote code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os, time
 
 while True:
     os.system("clear")
-    x = 
+    x = False
     r = RandomWord()
     word = r.word(word_min_length=6, word_max_length=6)
     print("welcome to wordle but with 6 letters instead of 5.")
@@ -16,9 +16,26 @@ while True:
         if guess not in checkguess:
             print("That's not a valid guess, try again")
             continue
+        if guess == r:
+            print("Great job!")
+            time.sleep(3)
+            quit()
         if guess[0] == r[0]:
             print(guess[0] + "Right letter, Right spot")
         elif guess[0] == r[1,2,3,4,5]:
             print(guess[0] + "Right letter, Wrong spot")
         elif guess[0] != r[0,1,2,3,4,5]:
-            
+            print(guess[0] + "Wrong letter, Wrong spot")
+        if guess[1] == r[1]:
+            print(guess[1] + "Right letter, Right spot")
+        elif guess[1] == r[0,2,3,4,5]:
+            print(guess[1] + "Right letter, Wrong spot")
+        elif guess[1] != r[0,1,2,3,4,5]:
+            print(guess[1] + "Wrong letter, Wrong spot")
+        if guess[2] == r[2]:
+            print(guess[2] + "Right letter, Right spot")
+        elif guess[2] == r[0,1,3,4,5]:
+            print(guess[0] + "Right letter, Wrong spot")
+        elif guess[2] != r[0,1,2,3,4,5]:
+            print(guess[2] + "Wrong letter, Wrong spot")
+        
