0982ae4137bc59b6bb08258a7410c11d7bd9bb9a
Inksplat58 <124327110+Inksplat58@users.noreply.github.com>
2024-04-02 09:17:58 -0400

    wrote code

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from wonderwords import RandomWord
 import os, time
 
-r = RandomWord()
-word = r.word(word_min_length=6, word_max_length=6)
-print("welcome to wordle but with 6 letters instead of 5")
-print("you have 7 guesses")
-print (word)
+while True:
+    os.system("clear")
+    r = RandomWord()
+    word = r.word(word_min_length=6, word_max_length=6)
+    print("welcome to wordle but with 6 letters instead of 5.")
+    print("You have 7 guesses.")
+    while True:
+        guess = input("Enter your first guess\n> ")
+        checkguess = w.filter(word_min_length=6, word_max_length=6, starts_with=guess)
+        if guess not in checkguess:
+            print("That's not a valid guess, try again")
+            continue
+        
