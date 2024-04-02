2350319013cd78da0aeb96e99876d73206f2a436
Inksplat58 <124327110+Inksplat58@users.noreply.github.com>
2024-04-02 15:07:01 -0400

    wrote code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import enchant
+import os, time
+from colorama import Fore, Back, Style
 import wonderwords
 from wonderwords import RandomWord
-import os, time
 
 while True:
     os.system("clear")
@@ -21,21 +21,43 @@ while True:
             time.sleep(3)
             quit()
         if guess[0] == r[0]:
-            print(guess[0] + "Right letter, Right spot")
+            print(Fore.GREEN + guess[0] + " Right letter, Right spot")
         elif guess[0] == r[1,2,3,4,5]:
-            print(guess[0] + "Right letter, Wrong spot")
+            print(Fore.YELLOW + guess[0] + " Right letter, Wrong spot")
         elif guess[0] != r[0,1,2,3,4,5]:
-            print(guess[0] + "Wrong letter, Wrong spot")
+            print(guess[0] + " Wrong letter, Wrong spot")
         if guess[1] == r[1]:
-            print(guess[1] + "Right letter, Right spot")
+            print(Fore.GREEN + guess[1] + " Right letter, Right spot")
         elif guess[1] == r[0,2,3,4,5]:
-            print(guess[1] + "Right letter, Wrong spot")
+            print(Fore.YELLOW + guess[1] + " Right letter, Wrong spot")
         elif guess[1] != r[0,1,2,3,4,5]:
-            print(guess[1] + "Wrong letter, Wrong spot")
+            print(guess[1] + " Wrong letter, Wrong spot")
         if guess[2] == r[2]:
-            print(guess[2] + "Right letter, Right spot")
+            print(Fore.GREEN + guess[2] + " Right letter, Right spot")
         elif guess[2] == r[0,1,3,4,5]:
-            print(guess[0] + "Right letter, Wrong spot")
+            print(Fore.YELLOW + guess[2] + " Right letter, Wrong spot")
         elif guess[2] != r[0,1,2,3,4,5]:
-            print(guess[2] + "Wrong letter, Wrong spot")
+            print(guess[2] + " Wrong letter, Wrong spot")
+        if guess[3] == r[3]:
+            print(Fore.GREEN + guess[3] + " Right letter, Right spot")
+        elif guess[3] == r[0,1,2,4,5]:
+            print(Fore.YELLOW + guess[0] + " Right letter, Wrong spot")
+        elif guess[3] != r[0,1,2,3,4,5]:
+            print(guess[3] + " Wrong letter, Wrong spot")
+        if guess[4] == r[4]:
+            print(Fore.GREEN + guess[4] + " Right letter, Right spot")
+        elif guess[4] == r[0,1,2,3,5]:
+            print(Fore.YELLOW + guess[4] + " Right letter, Wrong spot")
+        elif guess[4] != r[0,1,2,3,4,5]:
+            print(guess[4] + " Wrong letter, Wrong spot")
+        if guess[5] == r[5]:
+            print(Fore.GREEN + guess[5] + " Right letter, Right spot")
+            x = True
+        elif guess[5] == r[0,1,2,3,4]:
+            print(Fore.YELLOW + guess[5] + " Right letter, Wrong spot")
+            x = True
+        elif guess[5] != r[0,1,2,3,4,5]:
+            print(guess[5] + " Wrong letter, Wrong spot")
+            x = True
+    
         
