e268606f8fe4b893b4b4f5be4f8987332c6e0216
Inksplat58 <124327110+Inksplat58@users.noreply.github.com>
2024-06-05 18:56:02 +0000

    made answers easier to read

--- main.py
+++ main.py
@@ -75,44 +75,45 @@ while True:
             time.sleep(3)
             quit()
         if guess[0] == word[0]:
-            print(Fore.GREEN + guess[0])
+            letter1 = (Fore.GREEN + guess[0])
         elif guess[0] in word[1:6]:
-            print(Fore.YELLOW + guess[0])
+            letter1 = (Fore.YELLOW + guess[0])
         elif guess[0] not in word[0:6]:
-            print(Fore.WHITE + guess[0])
+            letter1 = (Fore.WHITE + guess[0])
         if guess[1] == word[1]:
-            print(Fore.GREEN + guess[1])
+            letter2 = (Fore.GREEN + guess[1])
         elif guess[1] in word[0:1] + word[2:6]:
-            print(Fore.YELLOW + guess[1])
+            letter2 = (Fore.YELLOW + guess[1])
         elif guess[1] not in word[0:6]:
-            print(Fore.WHITE + guess[1])
+            letter2 = (Fore.WHITE + guess[1])
         if guess[2] == word[2]:
-            print(Fore.GREEN + guess[2])
+            letter3 = (Fore.GREEN + guess[2])
         elif guess[2] in word[0:2] + word[3:6]:
-            print(Fore.YELLOW + guess[2])
+            letter3 = (Fore.YELLOW + guess[2])
         elif guess[2] not in word[0:6]:
-            print(Fore.WHITE + guess[2])
+            letter3 = (Fore.WHITE + guess[2])
         if guess[3] == word[3]:
-            print(Fore.GREEN + guess[3])
+            letter4 = (Fore.GREEN + guess[3])
         elif guess[3] in word[0:3] + word[4:6]:
-            print(Fore.YELLOW + guess[0])
+            letter4 = (Fore.YELLOW + guess[0])
         elif guess[3] not in word[0:6]:
-            print(Fore.WHITE + guess[3])
+            letter4 = (Fore.WHITE + guess[3])
         if guess[4] == word[4]:
-            print(Fore.GREEN + guess[4])
+            letter5 = (Fore.GREEN + guess[4])
         elif guess[4] in word[0:4] + word[5:6]:
-            print(Fore.YELLOW + guess[4])
+            letter5 = (Fore.YELLOW + guess[4])
         elif guess[4] not in word[0:6]:
-            print(Fore.WHITE + guess[4])
+            letter5 = (Fore.WHITE + guess[4])
         if guess[5] == word[5]:
-            print(Fore.GREEN + guess[5])
+            letter6 = (Fore.GREEN + guess[5])
             secondguess = True
-        elif guess[5] in word[0:5]:
-            print(Fore.YELLOW + guess[5])
+        elif guess[5] == word[0:5]:
+            letter6 = (Fore.YELLOW + guess[5])
             secondguess = True
         elif guess[5] not in word[0:6]:
-            print(Fore.WHITE + guess[5])
+            letter6 = (Fore.WHITE + guess[5])
             secondguess = True
+    print(letter1 + letter2 + letter3 + letter4 + letter5 + letter6)
     while not thirdguess:
         guess = input(Fore.WHITE + "Enter your third guess\n> ")
         checkguess = r.filter(word_min_length=6, word_max_length=6, starts_with=guess)
@@ -123,44 +124,45 @@ while True:
             time.sleep(3)
             quit()
         if guess[0] == word[0]:
-            print(Fore.GREEN + guess[0])
+            letter1 = (Fore.GREEN + guess[0])
         elif guess[0] in word[1:6]:
-            print(Fore.YELLOW + guess[0])
+            letter1 = (Fore.YELLOW + guess[0])
         elif guess[0] not in word[0:6]:
-            print(Fore.WHITE + guess[0])
+            letter1 = (Fore.WHITE + guess[0])
         if guess[1] == word[1]:
-            print(Fore.GREEN + guess[1])
+            letter2 = (Fore.GREEN + guess[1])
         elif guess[1] in word[0:1] + word[2:6]:
-            print(Fore.YELLOW + guess[1])
+            letter2 = (Fore.YELLOW + guess[1])
         elif guess[1] not in word[0:6]:
-            print(Fore.WHITE + guess[1])
+            letter2 = (Fore.WHITE + guess[1])
         if guess[2] == word[2]:
-            print(Fore.GREEN + guess[2])
+            letter3 = (Fore.GREEN + guess[2])
         elif guess[2] in word[0:2] + word[3:6]:
-            print(Fore.YELLOW + guess[2])
+            letter3 = (Fore.YELLOW + guess[2])
         elif guess[2] not in word[0:6]:
-            print(Fore.WHITE + guess[2])
+            letter3 = (Fore.WHITE + guess[2])
         if guess[3] == word[3]:
-            print(Fore.GREEN + guess[3])
+            letter4 = (Fore.GREEN + guess[3])
         elif guess[3] in word[0:3] + word[4:6]:
-            print(Fore.YELLOW + guess[0])
+            letter4 = (Fore.YELLOW + guess[0])
         elif guess[3] not in word[0:6]:
-            print(Fore.WHITE + guess[3])
+            letter4 = (Fore.WHITE + guess[3])
         if guess[4] == word[4]:
-            print(Fore.GREEN + guess[4])
+            letter5 = (Fore.GREEN + guess[4])
         elif guess[4] in word[0:4] + word[5:6]:
-            print(Fore.YELLOW + guess[4])
+            letter5 = (Fore.YELLOW + guess[4])
         elif guess[4] not in word[0:6]:
-            print(Fore.WHITE + guess[4])
+            letter5 = (Fore.WHITE + guess[4])
         if guess[5] == word[5]:
-            print(Fore.GREEN + guess[5])
+            letter6 = (Fore.GREEN + guess[5])
             thirdguess = True
-        elif guess[5] in word[0:5]:
-            print(Fore.YELLOW + guess[5])
+        elif guess[5] == word[0:5]:
+            letter6 = (Fore.YELLOW + guess[5])
             thirdguess = True
         elif guess[5] not in word[0:6]:
-            print(Fore.WHITE + guess[5])
+            letter6 = (Fore.WHITE + guess[5])
             thirdguess = True
+    print(letter1 + letter2 + letter3 + letter4 + letter5 + letter6)
     while not fourthguess:
         guess = input(Fore.WHITE + "Enter your fourth guess\n> ")
         checkguess = r.filter(word_min_length=6, word_max_length=6, starts_with=guess)
@@ -171,44 +173,45 @@ while True:
             time.sleep(3)
             quit()
         if guess[0] == word[0]:
-            print(Fore.GREEN + guess[0])
+            letter1 = (Fore.GREEN + guess[0])
         elif guess[0] in word[1:6]:
-            print(Fore.YELLOW + guess[0])
+            letter1 = (Fore.YELLOW + guess[0])
         elif guess[0] not in word[0:6]:
-            print(Fore.WHITE + guess[0])
+            letter1 = (Fore.WHITE + guess[0])
         if guess[1] == word[1]:
-            print(Fore.GREEN + guess[1])
+            letter2 = (Fore.GREEN + guess[1])
         elif guess[1] in word[0:1] + word[2:6]:
-            print(Fore.YELLOW + guess[1])
+            letter2 = (Fore.YELLOW + guess[1])
         elif guess[1] not in word[0:6]:
-            print(Fore.WHITE + guess[1])
+            letter2 = (Fore.WHITE + guess[1])
         if guess[2] == word[2]:
-            print(Fore.GREEN + guess[2])
+            letter3 = (Fore.GREEN + guess[2])
         elif guess[2] in word[0:2] + word[3:6]:
-            print(Fore.YELLOW + guess[2])
+            letter3 = (Fore.YELLOW + guess[2])
         elif guess[2] not in word[0:6]:
-            print(Fore.WHITE + guess[2])
+            letter3 = (Fore.WHITE + guess[2])
         if guess[3] == word[3]:
-            print(Fore.GREEN + guess[3])
+            letter4 = (Fore.GREEN + guess[3])
         elif guess[3] in word[0:3] + word[4:6]:
-            print(Fore.YELLOW + guess[0])
+            letter4 = (Fore.YELLOW + guess[0])
         elif guess[3] not in word[0:6]:
-            print(Fore.WHITE + guess[3])
+            letter4 = (Fore.WHITE + guess[3])
         if guess[4] == word[4]:
-            print(Fore.GREEN + guess[4])
+            letter5 = (Fore.GREEN + guess[4])
         elif guess[4] in word[0:4] + word[5:6]:
-            print(Fore.YELLOW + guess[4])
+            letter5 = (Fore.YELLOW + guess[4])
         elif guess[4] not in word[0:6]:
-            print(Fore.WHITE + guess[4])
+            letter5 = (Fore.WHITE + guess[4])
         if guess[5] == word[5]:
-            print(Fore.GREEN + guess[5])
+            letter6 = (Fore.GREEN + guess[5])
             fourthguess = True
-        elif guess[5] in word[0:5]:
-            print(Fore.YELLOW + guess[5])
+        elif guess[5] == word[0:5]:
+            letter6 = (Fore.YELLOW + guess[5])
             fourthguess = True
         elif guess[5] not in word[0:6]:
-            print(Fore.WHITE + guess[5])
+            letter6 = (Fore.WHITE + guess[5])
             fourthguess = True
+    print(letter1 + letter2 + letter3 + letter4 + letter5 + letter6)
     while not fifthguess:
         guess = input(Fore.WHITE + "Enter your fifth guess\n> ")
         checkguess = r.filter(word_min_length=6, word_max_length=6, starts_with=guess)
@@ -219,44 +222,45 @@ while True:
             time.sleep(3)
             quit()
         if guess[0] == word[0]:
-            print(Fore.GREEN + guess[0])
+            letter1 = (Fore.GREEN + guess[0])
         elif guess[0] in word[1:6]:
-            print(Fore.YELLOW + guess[0])
+            letter1 = (Fore.YELLOW + guess[0])
         elif guess[0] not in word[0:6]:
-            print(Fore.WHITE + guess[0])
+            letter1 = (Fore.WHITE + guess[0])
         if guess[1] == word[1]:
-            print(Fore.GREEN + guess[1])
+            letter2 = (Fore.GREEN + guess[1])
         elif guess[1] in word[0:1] + word[2:6]:
-            print(Fore.YELLOW + guess[1])
+            letter2 = (Fore.YELLOW + guess[1])
         elif guess[1] not in word[0:6]:
-            print(Fore.WHITE + guess[1])
+            letter2 = (Fore.WHITE + guess[1])
         if guess[2] == word[2]:
-            print(Fore.GREEN + guess[2])
+            letter3 = (Fore.GREEN + guess[2])
         elif guess[2] in word[0:2] + word[3:6]:
-            print(Fore.YELLOW + guess[2])
+            letter3 = (Fore.YELLOW + guess[2])
         elif guess[2] not in word[0:6]:
-            print(Fore.WHITE + guess[2])
+            letter3 = (Fore.WHITE + guess[2])
         if guess[3] == word[3]:
-            print(Fore.GREEN + guess[3])
+            letter4 = (Fore.GREEN + guess[3])
         elif guess[3] in word[0:3] + word[4:6]:
-            print(Fore.YELLOW + guess[0])
+            letter4 = (Fore.YELLOW + guess[0])
         elif guess[3] not in word[0:6]:
-            print(Fore.WHITE + guess[3])
+            letter4 = (Fore.WHITE + guess[3])
         if guess[4] == word[4]:
-            print(Fore.GREEN + guess[4])
+            letter5 = (Fore.GREEN + guess[4])
         elif guess[4] in word[0:4] + word[5:6]:
-            print(Fore.YELLOW + guess[4])
+            letter5 = (Fore.YELLOW + guess[4])
         elif guess[4] not in word[0:6]:
-            print(Fore.WHITE + guess[4])
+            letter5 = (Fore.WHITE + guess[4])
         if guess[5] == word[5]:
-            print(Fore.GREEN + guess[5])
+            letter6 = (Fore.GREEN + guess[5])
             fifthguess = True
-        elif guess[5] in word[0:5]:
-            print(Fore.YELLOW + guess[5])
+        elif guess[5] == word[0:5]:
+            letter6 = (Fore.YELLOW + guess[5])
             fifthguess = True
         elif guess[5] not in word[0:6]:
-            print(Fore.WHITE + guess[5])
+            letter6 = (Fore.WHITE + guess[5])
             fifthguess = True
+    print(letter1 + letter2 + letter3 + letter4 + letter5 + letter6)
     while not sixthguess:
         guess = input(Fore.WHITE + "Enter your sixth guess\n> ")
         checkguess = r.filter(word_min_length=6, word_max_length=6, starts_with=guess)
@@ -267,45 +271,47 @@ while True:
             time.sleep(3)
             quit()
         if guess[0] == word[0]:
-            print(Fore.GREEN + guess[0])
+            letter1 = (Fore.GREEN + guess[0])
         elif guess[0] in word[1:6]:
-            print(Fore.YELLOW + guess[0])
+            letter1 = (Fore.YELLOW + guess[0])
         elif guess[0] not in word[0:6]:
-            print(Fore.WHITE + guess[0])
+            letter1 = (Fore.WHITE + guess[0])
         if guess[1] == word[1]:
-            print(Fore.GREEN + guess[1])
+            letter2 = (Fore.GREEN + guess[1])
         elif guess[1] in word[0:1] + word[2:6]:
-            print(Fore.YELLOW + guess[1])
+            letter2 = (Fore.YELLOW + guess[1])
         elif guess[1] not in word[0:6]:
-            print(Fore.WHITE + guess[1])
+            letter2 = (Fore.WHITE + guess[1])
         if guess[2] == word[2]:
-            print(Fore.GREEN + guess[2])
+            letter3 = (Fore.GREEN + guess[2])
         elif guess[2] in word[0:2] + word[3:6]:
-            print(Fore.YELLOW + guess[2])
+            letter3 = (Fore.YELLOW + guess[2])
         elif guess[2] not in word[0:6]:
-            print(Fore.WHITE + guess[2])
+            letter3 = (Fore.WHITE + guess[2])
         if guess[3] == word[3]:
-            print(Fore.GREEN + guess[3])
+            letter4 = (Fore.GREEN + guess[3])
         elif guess[3] in word[0:3] + word[4:6]:
-            print(Fore.YELLOW + guess[0])
+            letter4 = (Fore.YELLOW + guess[0])
         elif guess[3] not in word[0:6]:
-            print(Fore.WHITE + guess[3])
+            letter4 = (Fore.WHITE + guess[3])
         if guess[4] == word[4]:
-            print(Fore.GREEN + guess[4])
+            letter5 = (Fore.GREEN + guess[4])
         elif guess[4] in word[0:4] + word[5:6]:
-            print(Fore.YELLOW + guess[4])
+            letter5 = (Fore.YELLOW + guess[4])
         elif guess[4] not in word[0:6]:
-            print(Fore.WHITE + guess[4])
+            letter5 = (Fore.WHITE + guess[4])
         if guess[5] == word[5]:
-            print(Fore.GREEN + guess[5])
+            letter6 = (Fore.GREEN + guess[5])
             sixthguess = True
-        elif guess[5] in word[0:5]:
-            print(Fore.YELLOW + guess[5])
+        elif guess[5] == word[0:5]:
+            letter6 = (Fore.YELLOW + guess[5])
             sixthguess = True
         elif guess[5] not in word[0:6]:
-            print(Fore.WHITE + guess[5])
+            letter6 = (Fore.WHITE + guess[5])
             sixthguess = True
-    while not seventhguess:
+    print(letter1 + letter2 + letter3 + letter4 + letter5 + letter6)
+    print ("\033[A                             \033[A")
+    while not Seventhguess:
         guess = input(Fore.WHITE + "Enter your seventh guess\n> ")
         checkguess = r.filter(word_min_length=6, word_max_length=6, starts_with=guess)
         if guess not in checkguess:     
@@ -315,43 +321,44 @@ while True:
             time.sleep(3)
             quit()
         if guess[0] == word[0]:
-            print(Fore.GREEN + guess[0])
+            letter1 = (Fore.GREEN + guess[0])
         elif guess[0] in word[1:6]:
-            print(Fore.YELLOW + guess[0])
+            letter1 = (Fore.YELLOW + guess[0])
         elif guess[0] not in word[0:6]:
-            print(Fore.WHITE + guess[0])
+            letter1 = (Fore.WHITE + guess[0])
         if guess[1] == word[1]:
-            print(Fore.GREEN + guess[1])
+            letter2 = (Fore.GREEN + guess[1])
         elif guess[1] in word[0:1] + word[2:6]:
-            print(Fore.YELLOW + guess[1])
+            letter2 = (Fore.YELLOW + guess[1])
         elif guess[1] not in word[0:6]:
-            print(Fore.WHITE + guess[1])
+            letter2 = (Fore.WHITE + guess[1])
         if guess[2] == word[2]:
-            print(Fore.GREEN + guess[2])
+            letter3 = (Fore.GREEN + guess[2])
         elif guess[2] in word[0:2] + word[3:6]:
-            print(Fore.YELLOW + guess[2])
+            letter3 = (Fore.YELLOW + guess[2])
         elif guess[2] not in word[0:6]:
-            print(Fore.WHITE + guess[2])
+            letter3 = (Fore.WHITE + guess[2])
         if guess[3] == word[3]:
-            print(Fore.GREEN + guess[3])
+            letter4 = (Fore.GREEN + guess[3])
         elif guess[3] in word[0:3] + word[4:6]:
-            print(Fore.YELLOW + guess[0])
+            letter4 = (Fore.YELLOW + guess[0])
         elif guess[3] not in word[0:6]:
-            print(Fore.WHITE + guess[3])
+            letter4 = (Fore.WHITE + guess[3])
         if guess[4] == word[4]:
-            print(Fore.GREEN + guess[4])
+            letter5 = (Fore.GREEN + guess[4])
         elif guess[4] in word[0:4] + word[5:6]:
-            print(Fore.YELLOW + guess[4])
+            letter5 = (Fore.YELLOW + guess[4])
         elif guess[4] not in word[0:6]:
-            print(Fore.WHITE + guess[4])
+            letter5 = (Fore.WHITE + guess[4])
         if guess[5] == word[5]:
-            print(Fore.GREEN + guess[5])
-            seventhguess = True
-        elif guess[5] in word[0:5]:
-            print(Fore.YELLOW + guess[5])
-            seventhguess = True
+            letter6 = (Fore.GREEN + guess[5])
+            Seventhguess = True
+        elif guess[5] == word[0:5]:
+            letter6 = (Fore.YELLOW + guess[5])
+            Seventhguess = True
         elif guess[5] not in word[0:6]:
-            print(Fore.WHITE + guess[5])
-            seventhguess = True
+            letter6 = (Fore.WHITE + guess[5])
+            Seventhguess = True
+    print(letter1 + letter2 + letter3 + letter4 + letter5 + letter6)
     print("You lost, the game will reset in 3 seconds")
     time.sleep(3)
